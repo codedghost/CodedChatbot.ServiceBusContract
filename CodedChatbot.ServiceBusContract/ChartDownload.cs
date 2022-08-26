2d93d8f2a331a3fc2fff35a62a73e5a722d2105e
codedghost2 <codedghost2@gmail.com>
2022-08-26 16:34:41 +0100

    Add project files.

---
+++ CodedChatbot.ServiceBusContract/ChartDownload.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CodedChatbot.ServiceBusContract
+{
+    public class ChartDownload
+    {
+        public string DownloadUrl { get; set; }
+        public string DirectoryName { get; set; }
+    }
+}
