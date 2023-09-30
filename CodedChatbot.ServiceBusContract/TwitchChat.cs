96c62ec8428f9d240dfbded4cd6d97e51c8c3e3a
codedghost2 <codedghost2@gmail.com>
2023-09-30 20:21:11 +0100

    Adding new TwitchChat model to service bus contract

---
+++ CodedChatbot.ServiceBusContract/TwitchChat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodedChatbot.ServiceBusContract
+{
+    public class TwitchChat
+    {
+        public string SendingApplication { get; set; }
+        public string Message { get; set; }
+    }
+}
